03a50c3dd8a935ff5bdafaf8ca6a7d91bfeac0d5
gfunk77 <faraci77@gmail.com>
2022-07-29 19:39:10 -0400

    toggle and closeBtn js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const toggle = document.querySelector(".toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+toggle.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
